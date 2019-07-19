69dbd36858d042cf91e456c6c7dc4b50bf9a7740
raviddog <raviddog@outlook.com>
2019-07-20 02:36:07 +1000

    Add project files.

---
+++ opengl winforms/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace opengl_winforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl1 = new OpenGL.GlControl();
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.glControl1.ColorBits = ((uint)(24u));
+            this.glControl1.DepthBits = ((uint)(0u));
+            this.glControl1.Location = new System.Drawing.Point(12, 12);
+            this.glControl1.MultisampleBits = ((uint)(0u));
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(445, 418);
+            this.glControl1.StencilBits = ((uint)(0u));
+            this.glControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.glControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OpenGL.GlControl glControl1;
+    }
+}
+
---
+++ opengl winforms/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Khronos;
+using OpenGL;
+
+
+namespace opengl_winforms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //assign event handlers to the proper functions
+            this.glControl1.ContextCreated += new System.EventHandler<OpenGL.GlControlEventArgs>(this.Render_CreateContext);
+            this.glControl1.ContextDestroying += new System.EventHandler<OpenGL.GlControlEventArgs>(this.Render_DestroyContext);
+            this.glControl1.Render += new System.EventHandler<OpenGL.GlControlEventArgs>(this.Render_UpdateContext);
+            this.glControl1.ContextUpdate += new System.EventHandler<OpenGL.GlControlEventArgs>(this.Render_OnUpdate);
+        }
+               
+        private void Render_CreateContext(object sender, GlControlEventArgs e)
+        {
+            //code executed when render context is created
+
+            GlControl glControl = (GlControl)sender;    //get control that triggered this
+            
+        }
+
+        private void Render_DestroyContext(object sender, GlControlEventArgs e)
+        {
+            //code executed when render context is destroyed
+        }
+
+        private void Render_OnUpdate(object sender, GlControlEventArgs e)
+        {
+            //code executed as the logic for each update i think?
+        }
+
+        private void Render_UpdateContext(object sender, GlControlEventArgs e)
+        {
+            //code executed to render the update iguess
+            //not sure how this is different to onupdate
+        }
+
+
+
+        //data to actually do stuf
+
+        private static float _Angle;
+
+        /// <summary>
+        /// Vertex position array.
+        /// </summary>
+        private static readonly float[] _ArrayPosition = new float[] {
+            0.0f, 0.0f,
+            1.0f, 0.0f,
+            1.0f, 1.0f
+        };
+
+        /// <summary>
+        /// Vertex color array.
+        /// </summary>
+        private static readonly float[] _ArrayColor = new float[] {
+            1.0f, 0.0f, 0.0f,
+            0.0f, 1.0f, 0.0f,
+            0.0f, 0.0f, 1.0f
+        };
+
+        //shaders
+
+        private readonly string[] _VertexShader = {
+            "#version 150 compatibility\n",
+            "uniform mat4 uMVP;\n",
+            "in vec2 aPosition;\n",
+            "in vec3 aColor;\n",
+            "out vec3 vColor;\n",
+            "void main() {\n",
+            "	gl_Position = uMVP * vec4(aPosition, 0.0, 1.0);\n",
+            "	vColor = aColor;\n",
+            "}\n"
+        };
+
+        private readonly string[] _FragmentShader = {
+            "#version 150 compatibility\n",
+            "in vec3 vColor;\n",
+            "void main() {\n",
+            "	gl_FragColor = vec4(vColor, 1.0);\n",
+            "}\n"
+        };
+
+    }
+}
